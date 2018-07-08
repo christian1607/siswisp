6dbe4c16115007d52e670edbae4dc6dd5b6298d8
Christian Altamirano Ayala <christian.altamirano.ayala@gmail.com>
2018-07-08 10:30:25 -0500

    datatype for users fixed

--- src/main/resources/db/migration/V1_2__security.sql
+++ src/main/resources/db/migration/V1_2__security.sql
@@ -10,15 +10,15 @@
 
 
 create table users(
-	username character(50) not null primary key,
-	password character(100) not null,
+	username varchar(50) not null primary key,
+	password varchar(60) not null,
 	enabled boolean not null
 );
 
 
 create table authorities (
-	username character(50) not null,
-	authority character(50) not null,
+	username varchar(50) not null,
+	authority varchar(50) not null,
 	constraint fk_authorities_users foreign key(username) references users(username)
 );
 create unique index ix_auth_username on authorities (username,authority);
